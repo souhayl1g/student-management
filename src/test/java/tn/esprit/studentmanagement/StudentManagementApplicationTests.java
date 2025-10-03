a484f72c26f74855d9720349cd8de321805ff659
souhayl1g <souhaylguenichi@gmail.com>
2025-10-03 04:24:39 +0100

    Fix : Updated Files for containerization

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,20 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+        // This test will pass if the application context loads successfully
     }
-
+    
+    @Test
+    void basicTest() {
+        // Simple test to verify the test environment works
+        assert true;
+    }
 }
